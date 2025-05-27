41cee005b1b65ba0e0d0fca32c146384f743a0b0
vijay <vijay.nvb@gmail.com>
2025-05-27 17:11:18 +0530

    http module

--- http_module/server.js
+++ http_module/server.js
@@ -1,21 +1,25 @@
 // listening on port 3000
 const http = require('http');
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end("root accessed");
-        return;
-    }else if(req.url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end("about accessed");
-        return;
-    } else if(req.url === '/contact') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end("contact accessed");
-        return;
-    }
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Path Not found!\n');
+const url = require('url');
+const server = http.createServer((req, res) => { 
+    
+    const parsedUrl = url.parse(req.url, true);
+    console.log(parsedUrl);
+    // if(req.url === '/') {
+    //     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //     res.end("root accessed");
+    //     return;
+    // }else if(req.url === '/about') {
+    //     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //     res.end("about accessed");
+    //     return;
+    // } else if(req.url === '/contact') {
+    //     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //     res.end("contact accessed");
+    //     return;
+    // }
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`welcome ${parsedUrl.query.name}!\n`);
 });
 
 server.listen(3000, () => {
